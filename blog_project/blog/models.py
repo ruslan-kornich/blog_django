dd58ac525c8a8d6fcd5da35c5ee4ef4826d87ebc
ruslan-kornich <korny4rus143@gmail.com>
2022-09-14 20:32:18 +0300

    register, users templates, urls

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
-
-# Create your models here.
+from django.utils import timezone
+from django.urls import reverse
 
 
 class Post(models.Model):
-    objects = models.Manager()
     title = models.CharField(max_length=200)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
@@ -15,4 +13,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
+    def get_absolute_url(self):
+        return reverse('blog-detail', kwargs={'pk': self.pk})
--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'users',
+    'crispy_forms',
 
 ]
 
